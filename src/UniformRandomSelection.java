dbb1bb9d6e061e81ff3f758c7fa5f7544467ef53
Kody Graham <kodydavison@hotmail.com>
2026-01-19 18:07:25 -0600

    Project outline transferred to project from instructions, ready to start coding.

--- src/UniformRandomSelection.java
+++ src/UniformRandomSelection.java
@@ -7,8 +7,26 @@
 
 class UniformRandomSelection {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("testsr");
 
+        //Indicated desired inputs for reference:
+        //% F = iris_bezdek.txt (name of data file)
+        //% K = 3 (number of clusters)
+        //% I = 100 (maximum number of iterations in a run)
+        //% T = 0.000001 (convergence threshold)
+        //% R = 100 (number of runs)
+        //% test is the name of the executable file
+        //% “>” indicates command-prompt, which is not part of the output
+
+        //Valid command prompt
+        //test iris_bezdek.txt 3 100 0.000001 100
+
+        //Example output format:
+        //5.1 3.4 1.5 0.2
+        //7.2 3.2 6 1.8
+        //4.6 3.1 1.5 0.2
+
+        //Four dimensions so 4 items in each row, 3 lines because k = 3
     }
 }
 
